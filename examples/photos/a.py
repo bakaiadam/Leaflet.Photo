6bb3cec711b04250dde2f85c9d82278564da9154
Your Name <you@example.com>
2018-05-28 07:26:46 +0200

    a

---
+++ examples/photos/a.py
@@ -0,0 +1,29 @@
+import subprocess
+import os
+import json
+
+s=[]
+
+for filename in os.listdir("."):
+    if (not filename.startswith("thumbnail") )  and (filename.endswith("JPG") or filename.endswith("PNG") or filename.endswith("jpg") or filename.endswith("png") ): 
+        # print(os.path.join(directory, filename))
+        g=subprocess.getoutput('exiftool -c "%.6f"  "'+filename+'"|egrep "GPS Position"')
+        #print(g)
+        h={}
+        g2=g.split()
+        #print("g2:"+filename,filename.startswith("thumbnail") )
+        #print(g2)
+        h["lat"]=g2[3]
+        h["lng"]=g2[5]
+        h["url"]="photos/"+filename
+        h["thumbnail"]="photos/thumbnail"+filename
+        h["caption"]=""        
+        h["name"]=""     
+        g=subprocess.getoutput('convert -thumbnail 80 '+filename+' thumbnail'+filename)
+        
+        s.append(h)
+        continue
+    else:
+        continue
+print("var photos="+json.dumps(s)+";")        
+
