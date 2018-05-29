ad08dfa41dafc091b4606d4a77b5d55120347b03
Your Name <you@example.com>
2018-05-29 08:12:29 +0200

    now some pictures are put into the same gallery.

--- examples/a.js
+++ examples/a.js
@@ -1,3 +1,26 @@
+	    //This function takes in latitude and longitude of two location and returns the distance between them as the crow flies (in km)
+    function calcCrow(lat1, lon1, lat2, lon2) 
+    {
+      var R = 6371; // km
+      var dLat = toRad(lat2-lat1);
+      var dLon = toRad(lon2-lon1);
+      var lat1 = toRad(lat1);
+      var lat2 = toRad(lat2);
+
+      var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+        Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2); 
+      var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+      var d = R * c;
+      return d;
+    }
+
+    // Converts numeric degrees to radians
+    function toRad(Value) 
+    {
+        return Value * Math.PI / 180;
+    }
+	
+	
 	var map = L.map('map', {
 	    maxZoom: 14
 	});
@@ -13,9 +36,20 @@
 	var photoLayer = L.photo.cluster().on('click', function(evt) {
 	    var photo = evt.layer.photo,
 	        template = '<img src="{url}" /></a><p>{caption}</p>';
-            console.log(photo.url);
+            console.log(photo);
+            lat=photo.lat;
+            lng=photo.lng;
+                        items2=[photo.url]
+            for (index = 0; index < photos.length; ++index) {
+            if (photo.url==photos[index].url) continue;
+    dist=calcCrow(lat,lng,photos[index].lat,photos[index].lng);
+    if (dist<1)
+      items2.push(photos[index].url)
+    
+}
+
             $.SimpleLightbox.open({
-    items: [photo.url]
+    items: items2
 });
 /*
 	    evt.layer.bindPopup(L.Util.template(template, photo), {
